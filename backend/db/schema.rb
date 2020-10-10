b4dfbd4d6755b6d75933e29708ee5d54403a650b
Riley Slayden <slaydenriley@gmail.com>
2020-10-10 10:25:17 -0600

    got associations working

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_161504) do
+ActiveRecord::Schema.define(version: 2020_10_10_161944) do
 
   create_table "listings", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_10_10_161504) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema.define(version: 2020_10_10_161504) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "listings", "users"
 end
