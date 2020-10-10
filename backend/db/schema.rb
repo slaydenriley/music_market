8bb57ac644d15a8c07fc4938b241d9a26e022fe6
Riley Slayden <slaydenriley@gmail.com>
2020-10-10 10:10:02 -0600

    rolled back migration to change names

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_160510) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.text "description"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
