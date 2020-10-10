a7f5143b6ee59fbfbbd63eb92d3ba3db1afc4547
Riley Slayden <slaydenriley@gmail.com>
2020-10-10 10:36:40 -0600

    began configuring users controller

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def show
+    user = User.find_by(id: params[:id])
+    render json: user.to_json
+  end
+
+  def index
+    users = User.all
+    render json: users.to_json
+  end
 end
