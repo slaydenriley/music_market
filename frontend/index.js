2f50ae39a521ac4708af99346fe420803ed332e0
Riley Slayden <slaydenriley@gmail.com>
2020-10-11 14:40:22 -0600

    Began building login

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,11 @@
+const loginForm = document.querySelector(".login-form");
+
+function hideLogin() {
+  loginForm.style.display = "none"
+};
+
+hideLogin()
+
 login.addEventListener('click', function showLogin() {
-  let login_form = document.querySelector('#login-form')
-  login_form.innerHTML = <form>Riley</form>
+  loginForm.style.display = "block"
 })
-
-function showRiley(riley) {
-  let test = document.querySelector('#login-form')
-  test.innerHTML = <
-}
-
-
-//document.querySelector('#login-form').innerText = users.name
