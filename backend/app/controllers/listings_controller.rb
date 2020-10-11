bdc2a8eedd1534cb114d06bf51c4b60a23340786
Riley Slayden <slaydenriley@gmail.com>
2020-10-11 13:30:30 -0600

    Configuring listings controller

--- backend/app/controllers/listings_controller.rb
+++ backend/app/controllers/listings_controller.rb
@@ -1,2 +1,11 @@
 class ListingsController < ApplicationController
+  def show
+    listing = Listing.find_by(id: params[:id])
+    render json: listing.to_json
+  end
+
+  def index
+    listings = Listing.all
+    render json: listings.to_json
+  end
 end
