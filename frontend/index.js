24841123d662cbae722363831d072a45a6721423
Riley Slayden <slaydenriley@gmail.com>
2020-10-11 15:58:06 -0600

    Building frontend login form

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,7 @@
 const loginForm = document.querySelector(".login-form");
 const signupForm = document.querySelector(".signup-form");
+const allUsers = document.querySelector(".all-users");
+const BACKEND_URL = "http://localhost:3000"
 
 function hideLogin() {
   loginForm.style.display = "none"
@@ -23,7 +25,55 @@ signup.addEventListener('click', function showSignup() {
 })
 
 users.addEventListener('click', function showUsers() {
-  fetch("http://localhost:3000/users")
+  fetch(`${BACKEND_URL}/users`)
   .then(users => users.json())
-  .then(users => console.log(users[0].name))
+  .then(users => renderUsers(users))
 })
+
+function renderUsers(users) {
+  allUsers.innerHTML = ""
+    users.forEach(user => {
+        allUsers.innerHTML += `<div>
+        <h3> ${user.name} <h3>
+
+      </div>`
+    })
+}
+
+function logIn(e) {
+    e.preventDefault();
+
+    let userInputForUsername = document.querySelector("#usernameForLogIn").value;
+
+    let formData = {
+        username: userInputForUsername
+    }
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+
+    fetch(`${BACKEND_URL}/login`, configObj)
+    .then(resp => resp.json())
+    .then(parsedResp => {
+        if (parsedResp.username) {
+
+            newComment(parsedResp.id);
+
+            const currentUser = document.querySelector("#current-user");
+            currentUser.innerText = parsedResp.username;
+
+            const highScore = document.querySelector("#levels-completed");
+            if (parsedResp.levels_completed === 0) {
+                highScore.innerText = `${parsedResp.levels_completed} levels completed`;
+            } else if (parsedResp.levels_completed === 1) {
+                highScore.innerText = `${parsedResp.levels_completed} level completed`;
+            }
+        }
+    });
+}
