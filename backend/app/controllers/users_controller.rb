a252c4de55d73029a4c70a5a0e5ad75098645bcd
Riley Slayden <slaydenriley@gmail.com>
2020-10-11 17:38:01 -0600

    Configured user serializer json

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def show
     user = User.find_by(id: params[:id])
-    render json: user.to_json
+    render json: UserSerializer.new(user).to_serialized_json
   end
 
   def index
     users = User.all
-    render json: users.to_json
+    render json: UserSerializer.new(user).to_serialized_json
   end
 end
--- backend/app/services/user_serializer.rb
+++ backend/app/services/user_serializer.rb
@@ -1,2 +1,9 @@
 class UserSerializer
+  def initialize(user_object)
+    @user = user_object
+  end
+
+  def to_serialized_json
+    @user.to_json(:only => [:name, :username, :email, :description])
+  end
 end
