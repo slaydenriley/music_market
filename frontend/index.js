596b892ded7cb8f8e77df2ebe10bd655ebce251b
Riley Slayden <slaydenriley@gmail.com>
2020-10-11 15:05:37 -0600

    Created login and signup screen with ajavascript and some CSS style

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,23 @@
 const loginForm = document.querySelector(".login-form");
+const signupForm = document.querySelector(".signup-form");
 
 function hideLogin() {
   loginForm.style.display = "none"
 };
 
+function hideSignup() {
+  signupForm.style.display = "none"
+}
+
 hideLogin()
+hideSignup()
 
 login.addEventListener('click', function showLogin() {
+  hideSignup()
   loginForm.style.display = "block"
+})
+
+signup.addEventListener('click', function showSignup() {
+  hideLogin()
+  signupForm.style.display = "block"
 })
