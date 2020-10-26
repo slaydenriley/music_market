d67f6be0f538adaa2ebd85c2ffafbbc53ee3da5e
Riley Slayden <slaydenriley@gmail.com>
2020-10-26 15:01:08 -0600

    Built user create controller action

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -8,4 +8,15 @@ class UsersController < ApplicationController
     users = User.all
     render json: UserSerializer.new(users).to_serialized_json
   end
+
+  def create
+    user = User.find_or_create_by(username: params[:username])
+    user.name = params[:name]
+    if params[:password] == params[:confirm_password]
+      user.password_digest = params[:password]
+      user.save
+    else
+      render plain: "Didn't work"
+    end
+  end
 end
