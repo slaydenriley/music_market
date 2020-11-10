9f1e40b67fd828d07c549d383ea5685f0bfe9eac
Riley Slayden <slaydenriley@gmail.com>
2020-11-10 10:27:39 -0700

    Configuring listings post

--- frontend/src/fetchers/listing_fetcher.js
+++ frontend/src/fetchers/listing_fetcher.js
@@ -11,5 +11,28 @@ class ListingFetcher {
 
   static post() {
     console.log("starting to post!")
+
+    let userInputForTitle = document.querySelector("#title").value;
+    let userInputForPrice = document.querySelector("#price").value;
+    let userInputForDescription = document.querySelector("#description").value;
+
+    let formData = {
+        title: userInputForTitle,
+        price: userInputForPrice,
+        description: userInputForPasswordConfirmation
+      };
+
+      let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          },
+          body: JSON.stringify(formData)
+        };
+
+      fetch(`${BACKEND_URL}/listings`, configObj)
+      .then(listing => listing.json())
+      .then(listing => console.log(listing))
   }
 }
