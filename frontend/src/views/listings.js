f1cd0e0d684887c5e6032e919c9452685f24389b
Riley Slayden <slaydenriley@gmail.com>
2020-11-10 14:37:40 -0700

    Commented code

--- frontend/src/views/listings.js
+++ frontend/src/views/listings.js
@@ -1,3 +1,4 @@
+// LISTING CLASS CALLED WHEN LOGGED IN FROM APP CLASS //
 class Listings {
   constructor() {
     this.listings = [];
@@ -8,30 +9,32 @@ class Listings {
     listings_button.addEventListener("click", function() {
       let fetchListings = new ListingFetcher
     });
-
     new_listing_button.addEventListener("click", function() {
       Listings.renderListingForm();
-      active_button = "new_listing_button"
     });
-
     newListingSubmit.addEventListener("click", function() {
       let postListing = ListingFetcher.post();
     });
   };
 
+// RENDERS ALL LISTINGS //
   static renderList(list) {
     App.clearMain();
     allListings.style.display = "block";
 
     list.forEach(listing => {
-      allListings.innerHTML += `<button class="card" id="${listing.id}">
-                                <h3>${listing.title}</h3> <h4>${listing.price}</h4>
-                                <p>${listing.description}</p></button>`;
+      let newHtml = `
+        <button class="card" id="${listing.id}">
+        <h3>${listing.title}</h3> <h4>${listing.price}</h4>
+        <p>${listing.description}</p></button>`;
+
+      allListings.innerHTML += newHtml
     });
 
     Listings.cardButtons()
   };
 
+// RENDERS THE NEW LISTING FORM //
   static renderListingForm() {
     App.clearMain();
     listingForm.style.display = "block";
@@ -44,6 +47,7 @@ class Listings {
                                 <p>${listing.description}</p></div>`;
   };
 
+// ADDS EVENT LISTENER TO ALL LISTING CARDS SO THEY CAN BE CLICKED //
   static cardButtons() {
     let card = document.querySelectorAll(".card")
     card.forEach(button => {
