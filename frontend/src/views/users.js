523b69ecb65d48c688d5b6b05ddf5235e4cb7c2b
Riley Slayden <slaydenriley@gmail.com>
2020-11-10 17:26:48 -0700

    Deleted old code

--- frontend/src/views/users.js
+++ frontend/src/views/users.js
@@ -33,31 +33,3 @@ class Users {
     singleUser.innerHTML += `<h3>${user.name}</h3> <h4>${user.username}</h4>`;
   };
 };
-
-/*
-function renderUser(user) {
-  App.clearMain()
-  fetch(`${BACKEND_URL}/users/${current_user_id}`)
-  .then(user => user.json())
-  .then(user => showUser(user))
-}
-
-function showUser(user) {
-  App.clearMain()
-  singleUser.innerHTML = `<h1>${user.name}</h1> <h2>${user.username}</h2> <button class="edit-account">Edit Account</button>`
-}
-
-function renderUsers(user) {
-  App.clearMain()
-  fetch(`${BACKEND_URL}/users/`)
-  .then(users => users.json())
-  .then(users => showUsers(users))
-}
-
-function showUsers(users) {
-  App.clearMain()
-  users.forEach(user => {
-    allUsers.innerHTML = `<h1>${user.name}</h1> <h2>${user.username}</h2> <button class="edit-account">Edit Account</button>`
-  })
-}
-*/
