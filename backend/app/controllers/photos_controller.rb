db71ee4ddb3472cb7df6a580795fae697d062269
Riley Slayden <slaydenriley@gmail.com>
2020-11-14 10:45:29 -0700

    Created photo controller

---
+++ backend/app/controllers/photos_controller.rb
@@ -0,0 +1,30 @@
+class PhotosController < ApplicationController
+
+  #Index action, photos gets listed in the order at which they were created
+  def index
+    @photos = Photo.order('created_at')
+  end
+
+  #New action for creating a new photo
+  def new
+    @photo = Photo.new
+  end
+
+  #Create action ensures that submitted photo gets created if it meets the requirements
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      json: {"does it work?"}
+    else
+
+    end
+  end
+
+  private
+
+  #Permitted parameters when creating a photo. This is used for security reasons.
+  def photo_params
+    params.require(:photo).permit(:title, :image)
+  end
+
+end
