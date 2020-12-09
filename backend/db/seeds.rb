6f09573bf7237c500b2c4180eacbf784dc49ed78
Riley Slayden <slaydenriley@gmail.com>
2020-12-09 12:43:47 -0700

    Added more seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -14,11 +14,45 @@ def User.digest(string)
   BCrypt::Password.create(string, cost: cost)
 end
 
-riley = User.create(name: "Riley Slayden", username: "rileyslayden", email: "slaydenriley@gmail.com", password_digest: "#{User.digest('codydog')}")
-morgan = User.create(name: "Morgan Hubbard", username: "morganhubbard",email: "mbhubbard@gmail.com", password_digest: "#{User.digest('codydog')}")
+## Create Users
+riley = User.create(
+  name: "Riley Slayden",
+  username: "rileyslayden",
+  email: "slaydenriley@gmail.com",
+  password_digest: "#{User.digest('codydog')}")
 
-banjo = riley.listings.build(title: "Banjo for Sale!", price: "$200", description: "Very nice banjo for sale!")
-fiddle = morgan.listings.build(title: "Fiddle for Sale", price: "$1000", description: "Very nice fiddle for sale!")
+morgan = User.create(
+  name: "Morgan Hubbard",
+  username: "morganhubbard",
+  email: "mbhubbard@gmail.com",
+  password_digest: "#{User.digest('codydog')}")
+
+maddi = User.create(
+  name: "Maddi Ogle",
+  username: "maddiogle",
+  email: "maddiogle@gmail.com",
+  password_digest: "#{User.digest('codydog')}")
+
+## Create Listings
+banjo = riley.listings.build(
+  title: "Banjo for Sale!",
+  price: "$200",
+  description: "Very nice banjo for sale!",
+  image_link: "https://d1aeri3ty3izns.cloudfront.net/media/54/543065/600/preview.jpg")
+
+fiddle = morgan.listings.build(
+  title: "Fiddle for Sale",
+  price: "$1000",
+  description: "Very nice fiddle for sale!",
+  image_link: "https://www.thestoryoftexas.com/upload/images/artifact-spotlight/2016-12-23-gimble-fiddle/gimble_fiddle_front.jpg")
+
+mando = maddi.listings.build(
+  title: "Weber Mandolin",
+  price: "$2000",
+  description: "Lightly used Weber mandolin. Built in Bozeman Montana!",
+  image_link: "https://cdn.shopify.com/s/files/1/0147/9412/9472/products/1_-_Copy_cfdddc51-223a-42bc-88ea-682e18b4f931_1024x1024.jpg?v=1589472828")
+
 
 banjo.save
 fiddle.save
+mando.save
