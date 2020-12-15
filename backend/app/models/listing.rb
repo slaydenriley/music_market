4abbfb8117736cca740627f946d6125e8afd098e
Riley Slayden <slaydenriley@gmail.com>
2020-12-15 11:26:56 -0700

    Building backend for favorites table

--- backend/app/models/listing.rb
+++ backend/app/models/listing.rb
@@ -1,5 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :user
-  
+  has_many :favorites
+  has_many :users, through: :favorites
+
   accepts_nested_attributes_for :user
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_14_200514) do
+ActiveRecord::Schema.define(version: 2020_12_15_182459) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "listing_id"
+  end
 
   create_table "listings", force: :cascade do |t|
     t.string "title"
