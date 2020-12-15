485f2fc3a3d86116555b9aa3e20c5b443a09d931
Riley Slayden <slaydenriley@gmail.com>
2020-12-15 11:54:05 -0700

    began creating fetch to add listing favorite

--- frontend/src/fetchers/favorite_fetcher.js
+++ frontend/src/fetchers/favorite_fetcher.js
@@ -2,11 +2,30 @@ class FavoriteFetcher {
   constructor() {
   }
 
-  static create_favorites() {
-    console.log("we are here")
+  static create_favorites(id) {
+    let formData = {
+      user_id: current_user_id,
+      listing_id: id
+    };
+
+     let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          },
+          body: JSON.stringify(formData)
+        };
+    fetch(`${BACKEND_URL}/favorites`, configObj)
+    .then(fav => fav.json())
+    .then(fav => console.log(fav))
   }
 
   static find_favorites() {
+
+  }
+
+  static delete_favorites() {
 
   }
 }
