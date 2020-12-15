7d9830124d0eae5d089055b1d69d4b49441ffbf9
Riley Slayden <slaydenriley@gmail.com>
2020-12-15 15:02:30 -0700

    Minor backend changes to favorite controller delete action

--- backend/app/controllers/favorites_controller.rb
+++ backend/app/controllers/favorites_controller.rb
@@ -14,9 +14,10 @@ class FavoritesController < ApplicationController
   end
 
   def delete
-    listing_id = params[:listing_id].to_i
-    favorite = Favorite.find_by(listing_id: listing_id)
-    favorite.delete
+    listing_id = favorite_params[:listing_id].to_i
+    user_id = favorite_params[:user_id]
+    favorite = Favorite.where(listing_id: params[:listing_id], user_id: params[:user_id])
+    Favorite.delete(favorite)
     render json: favorite
   end
 
