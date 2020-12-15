d1db9321559a832be0c42e203ea2c03c019de895
Riley Slayden <slaydenriley@gmail.com>
2020-12-15 11:59:48 -0700

    Created favorites controller and got create action working

--- backend/app/controllers/favorites_controller.rb
+++ backend/app/controllers/favorites_controller.rb
@@ -1,8 +1,22 @@
 class FavoritesController < ApplicationController
   def create
-    binding.pry
+    favorite = Favorite.new(favorite_params)
+    if favorite.save
+      render json: {user_id: favorite_params[:user_id], listing_id: favorite_params[:listing_id]}
+    else
+      binding.pry
+    end
   end
 
   def delete
+  end
+
+  private
+
+  def favorite_params
+    params.require(:favorite).permit(
+      :user_id,
+      :listing_id
+    )
   end
 end
